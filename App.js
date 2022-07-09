4fb121cf818c1e2b197f73a96d93e8a561a37839
pradeep kumar swain <54628915+pradeep2165@users.noreply.github.com>
2022-07-09 17:24:03 +0530

    home page made

--- App.js
+++ App.js
@@ -1,13 +1,22 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+// import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
 import Title from "./title/Title";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Title />
-      <Text>Pradeep</Text>
-      <StatusBar style="auto" />
+      <View style={styles.bannerContainer}>
+        <Image
+          style={styles.banner}
+          source={{
+            uri: "https://cdni.iconscout.com/illustration/premium/preview/online-quiz-5718736-4779390.png?w=0&h=1400",
+          }}
+        />
+      </View>
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.text}>Start</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -15,8 +24,26 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    // flexDirection: "column",
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  banner: {
+    height: 300,
+    width: 300,
+  },
+  button: {
+    backgroundColor: "#2874A6",
+    width: "40%",
+    paddingVertical: 16,
+    marginVertical: 16,
+    alignItems: "center",
+    borderRadius: 16,
+  },
+  text: {
+    color: "#EBF5FB",
+    fontSize: 20,
+    fontWeight: "600",
   },
 });
